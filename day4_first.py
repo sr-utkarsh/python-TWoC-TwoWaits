7ba0e531b4c98481134188ef9490d1e887e0c800
Utkarsh srivastava <sr.utkarsh19@gmail.com>
2020-05-16 20:30:51 +0530

    removing files

--- day4_first.py
+++
@@ -1,8 +0,0 @@
-n=int(input("Enter the no. of elements in the tuple "))
-print("Enter the elements of tuple ")
-arr=[]
-for i in range (n):
-    arr.append(input())
-arr=tuple(arr)
-ele=input("Enter the element whose count you wish to know ")
-print(ele,"occurs ",arr.count(ele)," times")
