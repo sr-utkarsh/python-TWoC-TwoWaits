502507e95afa47f8a3a8303eef7b322f0c931b49
Utkarsh srivastava <sr.utkarsh19@gmail.com>
2020-05-18 21:21:08 +0530

    removing files

--- day6_11th.py
+++
@@ -1,8 +0,0 @@
-n=int(input("Enter the size of list: "))
-A=[]
-print("Enter the elements: ")
-for i in range (n):
-    el=int(input())
-    A.append(el)
-A.sort()
-print("The highest product possible by multiplying 3 elements is: ",A[n-1]*A[n-2]*A[n-3])
